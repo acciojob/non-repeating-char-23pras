aeab1504e7004fdeca5b95aa26cd42a4ff2f8e2d
Prashant Pal <palprashant465@gmail.com>
2025-02-28 18:14:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(str){
+    str = str.toLowerCase();
+    if(str.length==0) return null;
+    let obj = {};
+    
+    for(let i=0; i<str.length; i++){
+        let ele = str[i];
+        if(obj[ele]){
+            obj[ele]++
+        }
+        else{
+            obj[ele]=1
+        }
+    }
+    for(let key in obj){
+        if(obj[key]==1) return key
+    }
+    return null
 }
+
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
